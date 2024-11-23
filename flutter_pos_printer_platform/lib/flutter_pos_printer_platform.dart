dc265bc40fb74a19587cf123c10c81b6583f4a7e
Partha Paul <partha.p_consultant@flocco.co>
2024-11-23 08:57:48 +0800

    add printer

---
+++ flutter_pos_printer_platform/lib/flutter_pos_printer_platform.dart
@@ -0,0 +1,12 @@
+library flutter_pos_printer_platform;
+
+// export './src/printers/escpos.dart';
+// export './src/printers/star.dart';
+// export './src/printers/tspl.dart';
+export './printer.dart';
+export './src/connectors/bluetooth.dart';
+export './src/connectors/tcp.dart';
+export './src/connectors/usb.dart';
+export './src/models/printer_device.dart';
+export './src/printer_manager.dart';
+export './src/utils.dart';
