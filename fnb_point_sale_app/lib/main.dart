b3bd6eb44e1209a72a346644f3b38005920e9c38
Partha Paul <partha.p_consultant@flocco.co>
2024-11-23 09:57:53 +0800

    printer

--- fnb_point_sale_app/lib/main.dart
+++ fnb_point_sale_app/lib/main.dart
@@ -13,11 +13,17 @@ import 'package:fnb_point_sale_base/data/remote/web_http_overrides.dart';
 import 'package:fnb_point_sale_base/locator.dart';
 import 'model/app_theme/my_app_theme.dart';
 import 'package:window_manager/window_manager.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+const hiveDbPath = 'fnb-pos-data';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HttpOverrides.global = WebHttpOverrides();
+  await EasyLocalization.ensureInitialized();
+  await Hive.initFlutter(hiveDbPath);
   if (Platform.isAndroid) {
     // Android-specific code
   } else if (Platform.isIOS) {
